1f4aa95df6202da920d0820baf00091252451319
aleskandro <aleskandro@seraph>
2019-04-12 12:37:37 +0200

    Timing graphs

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import math
 import pandas as pd
 import matplotlib.pyplot as plt
 import os
+import time
 
 from Generator.Generator import *
 from Generator.GeneratorBwConcave import *
@@ -40,27 +41,36 @@ cpuReq = UniformRandomVariable(0, K * (avgCpu * avgServers) / (avgContainers * a
 def simpleHeuristic(maxOpt, make_graph=True):
     bwOpts2 = pd.DataFrame(columns=["BandwidthSaving", "Options"])
     rrOpts2 = pd.DataFrame(columns=["Options", "CPU", "RAM"])
+    timing = pd.DataFrame(columns=["Options", "Time"])
     for i in range(1, maxOpt + 1):
         options = UniformRandomVariable(i, i)
         generator = GeneratorForModel(servers, serviceProviders,
                                       options, containers, [cpu, ram], bandwidth, [cpuReq, ramReq])
         generator.generate()  # TODO make multithread by not using a singleton (can I?)
         npp = NetworkProvider().getInstance()
+        t1 = time.time()
         npp.makePlacement(i)
+        t2 = time.time()
+        timing.loc[len(timing)] = {"Options": i, "Time": t2 - t1}
         bwOpts2.loc[len(bwOpts2)] = {"Options": i, "BandwidthSaving": npp.getBandwidthSaving()}
         rrOpts2.loc[len(rrOpts2)] = {"Options": i, "CPU": npp.getRemainingResources()[0], "RAM": npp.getRemainingResources()[1]}
     if make_graph:
-        makeGraph(bwOpts2, rrOpts2)
-    return bwOpts2, rrOpts2
+        makeGraph(bwOpts2, rrOpts2, timing)
+    return bwOpts2, rrOpts2, timing
 
 def simple(maxOpt):
+    timing = pd.DataFrame(columns=["Options", "Time"])
     for i in range(1, maxOpt + 1): 
         options = UniformRandomVariable(i, i)
         # generator = Generator(servers, serviceProviders, options, containers, [ram, cpu], bandwidth, [ramReq, cpuReq])
         generator = GeneratorBwConcave(servers, serviceProviders, options, containers, [cpu, ram], bandwidth, [cpuReq, ramReq])
 
         generator.generate()
+        t1 = time.time()
         os.system("glpsol --math modelglpk.mod -d scenario.dat")
+        t2 = time.time()
+        timing.loc[len(timing)] = {"Options": i, "Time": t2-t1}
+    return timing
 
 def confidenceInterval(x):
     std  = x.std()
@@ -80,10 +90,12 @@ def confidenceIntervalMin(x):
     count= x.count()
     return mean - 1.96*std/math.sqrt(count)
 
-def makeGraph(bwOpts, rrOpts):
+def makeGraph(bwOpts, rrOpts, timing):
     bwOpts = bwOpts.groupby("Options").agg([np.mean, confidenceInterval])
     rrOpts = rrOpts.groupby("Options").agg([np.mean, confidenceInterval])
-    fig, axs = plt.subplots(nrows=2, ncols=1)
+    timing = timing.groupby("Options").agg([np.mean, confidenceInterval])
+
+    fig, axs = plt.subplots(nrows=3, ncols=1)
     ax = axs[0]
     ax.set_ylim([0, math.ceil(bwOpts["BandwidthSaving"]["mean"].max())])
     ax.errorbar(bwOpts.index.values, bwOpts["BandwidthSaving"]["mean"], yerr=bwOpts["BandwidthSaving"]["confidenceInterval"], label="Bandwidth saving")
@@ -93,15 +105,20 @@ def makeGraph(bwOpts, rrOpts):
     ax.errorbar(rrOpts.index.values, rrOpts["RAM"]["mean"], yerr=rrOpts["RAM"]["confidenceInterval"], label="RAM")
     ax.legend(loc="best")
     ax.set_ylim([0, 1])
+    ax = axs[2]
+    ax.errorbar(timing.index.values, timing["Time"]["mean"], yerr=timing["Time"]["mean"], label="Time elapsed")
+    ax.legend(loc="best")
     fig.savefig("results/output.png")
 
-def makeGraphTogether(bwOptsILP, rrOptsILP, bwOptsH, rrOptsH):
+def makeGraphTogether(bwOptsILP, rrOptsILP, timingILP, bwOptsH, rrOptsH, timingH):
     bwOptsILP = bwOptsILP.groupby("Options").agg([np.mean, confidenceInterval])
     rrOptsILP = rrOptsILP.groupby("Options").agg([np.mean, confidenceInterval])
     bwOptsH["BandwidthSaving"] = bwOptsH["BandwidthSaving"].astype(float)
     bwOptsH = bwOptsH.groupby("Options").agg([np.mean, confidenceInterval])
     rrOptsH = rrOptsH.groupby("Options").agg([np.mean, confidenceInterval])
-    fig, axs = plt.subplots(nrows=2, ncols=1)
+    timingILP = timingILP.groupby("Options").agg([np.mean, confidenceInterval])
+    timingH = timingH.groupby("Options").agg([np.mean, confidenceInterval])
+    fig, axs = plt.subplots(nrows=3, ncols=1)
     ax = axs[0]
     ax.set_ylim([0, avgServiceProviders])  # math.ceil(bwOptsILP["BandwidthSaving"]["mean"].max())])
     ax.errorbar(bwOptsILP.index.values, bwOptsILP["BandwidthSaving"]["mean"], yerr=bwOptsILP["BandwidthSaving"]["confidenceInterval"], label="Bandwidth saving (optimal)")
@@ -114,35 +131,42 @@ def makeGraphTogether(bwOptsILP, rrOptsILP, bwOptsH, rrOptsH):
     ax.errorbar(rrOptsH.index.values, rrOptsH["RAM"]["mean"], yerr=rrOptsH["RAM"]["confidenceInterval"], label="RAM (heuristic)")
     ax.legend(loc="best")
     ax.set_ylim([0, 1])
+    ax = axs[2]
+    ax.errorbar(timingILP.index.values, timingILP["Time"]["mean"], yerr=timingILP["Time"]["confidenceInterval"], label="Time elapsed (optimal)")
+    ax.errorbar(timingH.index.values, timingH["Time"]["mean"], yerr=timingH["Time"]["confidenceInterval"], label="Time elapsed (heuristic)")
+    ax.legend(loc="best")
     fig.savefig("results/output.png")
 
 def grouped(runs, maxOpt, make_graph = True):
+    timingg = pd.DataFrame(columns=["Options", "Time"])
     for i in range(0, runs):
         Random.seed(i)
-        simple(maxOpt)
+        timing = simple(maxOpt)
+        timingg = timingg.append(timing)
     bwOpts = pd.read_csv("results/bandwidthByAvgOptions.csv", header=None, names=["Options","BandwidthSaving"])
     rrOpts = pd.read_csv("results/remainingResourcesByAvgOptions.csv", header=None, names=["Options", "CPU", "RAM"])
     if make_graph:
-        makeGraph(bwOpts, rrOpts)
-    return bwOpts, rrOpts
+        makeGraph(bwOpts, rrOpts, timingg)
+    return bwOpts, rrOpts, timingg
 
 def groupedHeuristic(runs, maxOpts, make_graph = True):
     bwOptss = pd.DataFrame(columns=["Options", "BandwidthSaving"])
     rrOptss = pd.DataFrame(columns=["Options", "CPU", "RAM"])
-
+    timingg = pd.DataFrame(columns=["Options", "Time"])
     for i in range(0, runs):
         Random.seed(i)
-        bwOpts, rrOpts = simpleHeuristic(maxOpts, False)
+        bwOpts, rrOpts, timing = simpleHeuristic(maxOpts, False)
         bwOptss = bwOptss.append(bwOpts)
         rrOptss = rrOptss.append(rrOpts)
+        timingg = timing.append(timing)
     if make_graph:
-        makeGraph(bwOptss, rrOptss)
-    return bwOptss, rrOptss
+        makeGraph(bwOptss, rrOptss, timingg)
+    return bwOptss, rrOptss, timingg
 
 def groupedTogether(runs, maxOpts):
-    bwOptsILP, rrOptsILP = grouped(runs, maxOpts, False)
-    bwOptsH, rrOptsH = groupedHeuristic(runs, maxOpts, False)
-    makeGraphTogether(bwOptsILP, rrOptsILP, bwOptsH, rrOptsH)
+    bwOptsILP, rrOptsILP, timingILP = grouped(runs, maxOpts, False)
+    bwOptsH, rrOptsH, timingH = groupedHeuristic(runs, maxOpts, False)
+    makeGraphTogether(bwOptsILP, rrOptsILP, timingILP, bwOptsH, rrOptsH, timingH)
 
 os.system("rm -rf results/*")
 #simple(200)
