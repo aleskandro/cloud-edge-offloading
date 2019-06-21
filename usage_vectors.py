fb679d3098c2df974130a1b460e4c2765997d764
aleskandro <aleskandro@seraph>
2019-06-21 13:02:11 +0200

    Usage vectors graph

---
+++ usage_vectors.py
@@ -0,0 +1,80 @@
+import static_cmpilph as cmpilph
+import numpy.random as Random
+from Model.NetworkProvider import NetworkProvider
+from Generator.GeneratorForModel import GeneratorForModel
+import pandas as pd
+import glob
+import matplotlib.pyplot as plt
+import cycler
+plt.rcParams.update({'font.size': 12.5, 'font.family': 'serif'})
+
+def usage_vectors(simulate, naive=False):
+    Random.seed(2)
+    cmpilph.generate_input_datas(K=2)
+    generator = GeneratorForModel(cmpilph.servers, cmpilph.serviceProviders,
+                                  cmpilph.options, cmpilph.containers, [cmpilph.cpu, cmpilph.ram], cmpilph.bandwidth, [cmpilph.cpuReq, cmpilph.ramReq])
+    generator.generate()  # TODO make multithread by not using a singleton (can I?)
+    npp = NetworkProvider().getInstance()
+    if not simulate:
+        return
+    if naive:
+        npp.make_placement_naive()
+    else:
+        npp.makePlacement(1)
+
+    df = pd.DataFrame(columns=["Node", "CPU", "RAM"])
+    for i, sp in enumerate(npp.getServiceProviders()):
+        if sp.getDefaultOption() is None:
+            continue
+
+        for container in sp.getDefaultOption().getContainers():
+            cpu = container.getCpuReq()
+            ram = container.getRamReq()
+            node = npp.getServers().index(container.getServer())
+            df.loc[len(df)] = {"Node": node, "CPU": cpu, "RAM": ram}
+    if naive:
+        df.to_csv("results/usage_vectors_naive.csv")
+    else:
+        df.to_csv("results/usage_vectors.csv")
+
+
+
+def usage_vectors_graph():
+    cmpilph.generate_input_datas()
+    simulate = len(glob.glob("results/usage_vectors.csv")) == 0
+    usage_vectors(simulate)
+    simulate = len(glob.glob("results/usage_vectors_naive.csv")) == 0
+    usage_vectors(simulate, True)
+    #monochrome = (cycler('color', ['k']) * cycler('linestyle', ['-', '--', ':']) * cycler('marker',['^', ',', '.']))
+    #for ax in axs:
+    #    ax.set_prop_cycle(monochrome)
+
+    df = pd.read_csv("results/usage_vectors.csv")
+    df_naive = pd.read_csv("results/usage_vectors_naive.csv")
+    fig, axs = plt.subplots(nrows=int(df["Node"].max()) + 1, ncols=1, figsize=(10,60))
+    def quiv(df, color="r"):
+        last_coordinate = [(0,0)] * (int(df["Node"].max()) + 1)
+        for index, row in df.iterrows():
+            ax = axs[int(row["Node"])]
+            ax.quiver([last_coordinate[int(row["Node"])][0]],
+                      [last_coordinate[int(row["Node"])][1]],
+                      [row["CPU"]],
+                      [row["RAM"]], angles='xy', scale_units='xy', scale=1, width=0.0025, color=[color])
+            last_coordinate[int(row["Node"])] = (last_coordinate[int(row["Node"])][0]
+                                                 + row["CPU"], last_coordinate[int(row["Node"])][1] + row["RAM"])
+    quiv(df, "r")
+    quiv(df_naive, "b")
+    #axs[0].quiver([0], [0], [1], [1], angles='xy', scale_units='xy', scale=1)
+    #axs[0].quiver([1], [1], [2], [3], angles='xy', scale_units='xy', scale=1)
+    #axs[0].quiver([3], [4], [4], [9], angles='xy', scale_units='xy', scale=1)
+    #axs[0].set_xlim(0, 10)
+    #axs[0].set_ylim(0, 10)
+    for i,ax in enumerate(axs):
+        ax.set_ylim(0,NetworkProvider().getInstance().getServers()[i].getTotalRam())
+        ax.set_xlim(0,NetworkProvider().getInstance().getServers()[i].getTotalCpu())
+    fig.savefig("results/usage_vectors.pdf", dpi=300)
+
+
+if __name__ == "__main__":
+    usage_vectors_graph()
+
